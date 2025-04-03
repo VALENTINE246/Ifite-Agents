efc1548bc27f21dc571aae63d64722a0f85b986a
VALENTINE246 <somtoifeduba2@gmail.com>
2025-04-03 22:00:31 +0100

    Add files via upload

---
+++ dashboard.js
@@ -0,0 +1,382 @@
+const student_1 = {
+  name: ' Ebede Raluchukwu',
+  department: ' Business Admin',
+  level: ' 300',
+  phone: '<a href="https://wa.me/+2349027317725"> 09027317725  </a>',
+  reg: ' 2022424016',
+  lodgeAdress: '',
+  status: 'Active'
+} 
+
+const name1 = document.getElementById('name-1').innerHTML = student_1.name;
+const dept1 = document.getElementById('dept-1').innerHTML = student_1.department;
+const level1 = document.getElementById('level-1').innerHTML = student_1.level;
+const phoneNumber1 = document.getElementById('phone-number-1').innerHTML = student_1.phone;
+const reg1 = document.getElementById('reg-1').innerHTML = student_1.reg
+const ActiveStatus1  = document.getElementById('stat1').innerHTML = student_1.status
+
+
+// Nnodim Deborah Chizaram - 2
+
+const student_2 = {
+  name: ' Nnodim Deborah Chizaram',
+  department: ' Linguistics',
+  level: ' 400',
+  phone: '<a href="https://wa.me/+2349027317725"> 09121074042  </a>',
+  reg: ' 2021054003',
+  status: 'Active'
+} 
+
+const name2 = document.getElementById('name-2').innerHTML = student_2.name;
+const dept2 = document.getElementById('dept-2').innerHTML = student_2.department;
+const level2 = document.getElementById('level-2').innerHTML = student_2.level;
+const phoneNumber2 = document.getElementById('phone-number-2').innerHTML = student_2.phone;
+const reg2 = document.getElementById('reg-2').innerHTML = student_2.reg
+const ActiveStatus2  = document.getElementById('stat2').innerHTML = student_2.status
+
+
+// Mbanefo Obumuneme - 3
+
+const student_3 = {
+  name: '  Mbanefo Obumuneme Linus',
+  department: ' Vocational&Tech',
+  level: ' ',
+  phone: '<a href="https://wa.me/+2349156336663"> 09156336663</a>',
+  reg: ' 2020164028',
+  status: 'Active'
+} 
+
+const name3 = document.getElementById('name-3').innerHTML = student_3.name;
+const dept3 = document.getElementById('dept-3').innerHTML = student_3.department;
+const level3 = document.getElementById('level-3').innerHTML = student_3.level;
+const phoneNumber3 = document.getElementById('phone-number-3').innerHTML = student_3.phone;
+const reg3 = document.getElementById('reg-3').innerHTML = student_3.reg
+const ActiveStatus3  = document.getElementById('stat3').innerHTML = student_3.status
+
+
+// Onyebuchi Praise - 4
+
+const student_4 = {
+  name: '  Onyebuchi Praise Chisom',
+  department: ' Mass Commmunication',
+  level: ' ',
+  phone: '<a href="https://wa.me/+2348164854786">  08164854786</a>',
+  reg: ' 2022124001',
+  status: 'Active'
+} 
+
+const name4 = document.getElementById('name-4').innerHTML = student_4.name;
+const dept4 = document.getElementById('dept-4').innerHTML = student_4.department;
+const level4 = document.getElementById('level-4').innerHTML = student_4.level;
+const phoneNumber4 = document.getElementById('phone-number-4').innerHTML = student_4.phone;
+const reg4 = document.getElementById('reg-4').innerHTML = student_4.reg
+const ActiveStatus4  = document.getElementById('stat4').innerHTML = student_4.status
+
+
+// Adolphus Arukwala - 5
+
+const student_5 = {
+  name: '  Adolphus Arukwala',
+  department: ' Linguistics',
+  level: ' ',
+  phone: '<a href="https://wa.me/+2348141869686">  08141869686</a>',
+  reg: ' 2020054120',
+  status: 'Active'
+} 
+
+const name5 = document.getElementById('name-5').innerHTML = student_5.name;
+const dept5 = document.getElementById('dept-5').innerHTML = student_5.department;
+const level5 = document.getElementById('level-5').innerHTML = student_5.level;
+const phoneNumber5 = document.getElementById('phone-number-5').innerHTML = student_5.phone;
+const reg5 = document.getElementById('reg-5').innerHTML = student_5.reg
+const ActiveStatus5  = document.getElementById('stat5').innerHTML = student_5.status
+
+// Ufonna Kizito - 6
+const student_6 = {
+  name: '  Ufonna Kizito',
+  department: ' Philosophy',
+  level: ' ',
+  phone: '<a href="https://wa.me/+2349165668308">  09165668308</a>',
+  reg: ' 2022084014',
+  status: 'Active'
+} 
+
+const name6 = document.getElementById('name-6').innerHTML = student_6.name;
+const dept6 = document.getElementById('dept-6').innerHTML = student_6.department;
+const level6 = document.getElementById('level-6').innerHTML = student_6.level;
+const phoneNumber6 = document.getElementById('phone-number-6').innerHTML = student_6.phone;
+const reg6 = document.getElementById('reg-6').innerHTML = student_6.reg
+const ActiveStatus6  = document.getElementById('stat6').innerHTML = student_6.status
+
+
+// Odurukwe Destiny Chijioke - 7
+
+const student_7 = {
+  name: '  Odurukwe Destiny Chijioke',
+  department: ' Building',
+  level: ' ',
+  phone: '<a href="https://wa.me/+2349151917147">  09151917147</a>',
+  reg: ' 2015284033',
+  status: 'Active'
+} 
+
+const name7 = document.getElementById('name-7').innerHTML = student_7.name;
+const dept7 = document.getElementById('dept-7').innerHTML = student_7.department;
+const level7 = document.getElementById('level-7').innerHTML = student_7.level;
+const phoneNumber7 = document.getElementById('phone-number-7').innerHTML = student_7.phone;
+const reg7 = document.getElementById('reg-7').innerHTML = student_7.reg
+const ActiveStatus7  = document.getElementById('stat7').innerHTML = student_7.status
+
+
+// Okechukwu Victor Chidindu - 8
+const student_8 = {
+  name: '  Okechukwu Victor Chidindu',
+  department: ' Linguistics',
+  level: ' ',
+  phone: '<a href="https://wa.me/+2348038272297">  08038272297</a>',
+  reg: ' 2023054147',
+  status: 'Active'
+} 
+
+const name8 = document.getElementById('name-8').innerHTML = student_8.name;
+const dept8 = document.getElementById('dept-8').innerHTML = student_8.department;
+const level8 = document.getElementById('level-8').innerHTML = student_8.level;
+const phoneNumber8 = document.getElementById('phone-number-8').innerHTML = student_8.phone;
+const reg8 = document.getElementById('reg-8').innerHTML = student_8.reg
+const ActiveStatus8  = document.getElementById('stat8').innerHTML = student_8.status
+
+
+// Nwaka Chigozirim - 9
+
+const student_9 = {
+  name: '  Nwaka Chigozirim',
+  department: ' Marketing',
+  level: ' 300',
+  phone: '<a href="https://wa.me/+2348023932310">  08023932310</a>',
+  reg: ' 2022444063',
+  status: 'Active'
+} 
+
+const name9 = document.getElementById('name-9').innerHTML = student_9.name;
+const dept9 = document.getElementById('dept-9').innerHTML = student_9.department;
+const level9 = document.getElementById('level-9').innerHTML = student_9.level;
+const phoneNumber9 = document.getElementById('phone-number-9').innerHTML = student_9.phone;
+const reg9 = document.getElementById('reg-9').innerHTML = student_9.reg
+const ActiveStatus9  = document.getElementById('stat9').innerHTML = student_9.status
+
+
+// Muoka Dorathy Ujunwa - 10
+
+const student_10 = {
+  name: '  Muoka Dorathy Ujunwa',
+  department: ' Mathematics',
+  level: ' ',
+  phone: '<a href="https://wa.me/+2348025491614">  08025491614</a>',
+  reg: ' 2023524009',
+  status: 'Active'
+} 
+
+const name10 = document.getElementById('name-10').innerHTML = student_10.name;
+const dept10 = document.getElementById('dept-10').innerHTML = student_10.department;
+const level10 = document.getElementById('level-10').innerHTML = student_10.level;
+const phoneNumber10 = document.getElementById('phone-number-10').innerHTML = student_10.phone;
+const reg10 = document.getElementById('reg-10').innerHTML = student_10.reg
+const ActiveStatus10  = document.getElementById('stat10').innerHTML = student_10.status
+
+
+// Clif Precious Chukwuma - 11
+
+const student_11 = {
+  name: '  Clif Precious Chukwuma',
+  department: ' Guidance and Counseling',
+  level: ' 300',
+  phone: '<a href="https://wa.me/+2347086393632">  07086393632</a>',
+  reg: ' 2022694041',
+  status: 'Active'
+} 
+
+const name11 = document.getElementById('name-11').innerHTML = student_11.name;
+const dept11 = document.getElementById('dept-11').innerHTML = student_11.department;
+const level11 = document.getElementById('level-11').innerHTML = student_11.level;
+const phoneNumber11 = document.getElementById('phone-number-11').innerHTML = student_11.phone;
+const reg11 = document.getElementById('reg-11').innerHTML = student_11.reg
+const ActiveStatus11  = document.getElementById('stat11').innerHTML = student_11.status
+
+
+// Eze Emmanuel Ezekiel - 12
+const student_12 = {
+  name: '  Eze Emmanuel Ezekiel',
+  department: ' Mechanical Engineering',
+  level: ' 300',
+  phone: '<a href="https://wa.me/+2347026315205">  07026315205</a>',
+  reg: ' 2023244004',
+  status: 'Active'
+} 
+
+const name12 = document.getElementById('name-12').innerHTML = student_12.name;
+const dept12 = document.getElementById('dept-12').innerHTML = student_12.department;
+const level12 = document.getElementById('level-12').innerHTML = student_12.level;
+const phoneNumber12 = document.getElementById('phone-number-12').innerHTML = student_12.phone;
+const reg12 = document.getElementById('reg-12').innerHTML = student_12.reg
+const ActiveStatus12  = document.getElementById('stat12').innerHTML = student_12.status
+
+
+// Chikezie Ifeanyi - 13
+const student_13 = {
+  name: '  Chikezie Ifeanyi',
+  department: ' Business Administration',
+  level: ' Graduate',
+  phone: '<a href="https://wa.me/+2349042570696">  09042570696</a>',
+  reg: ' 2020424157',
+  status: 'Active'
+} 
+
+const name13 = document.getElementById('name-13').innerHTML = student_13.name;
+const dept13 = document.getElementById('dept-13').innerHTML = student_13.department;
+const level13 = document.getElementById('level-13').innerHTML = student_13.level;
+const phoneNumber13 = document.getElementById('phone-number-13').innerHTML = student_13.phone;
+const reg13 = document.getElementById('reg-13').innerHTML = student_13.reg
+const ActiveStatus13  = document.getElementById('stat13').innerHTML = student_13.status
+
+
+// Fred Wisdom Chigozie - 14
+const student_14 = {
+  name: '  Fred Wisdom Chigozie',
+  department: ' Enterprenueurship Studies',
+  level: ' Graduate',
+  phone: '<a href="https://wa.me/+2349056378189">  09056378189</a>',
+  reg: ' 2019804019',
+  status: 'Active'
+} 
+
+const name14 = document.getElementById('name-14').innerHTML = student_14.name;
+const dept14 = document.getElementById('dept-14').innerHTML = student_14.department;
+const level14 = document.getElementById('level-14').innerHTML = student_14.level;
+const phoneNumber14 = document.getElementById('phone-number-14').innerHTML = student_14.phone;
+const reg14 = document.getElementById('reg-14').innerHTML = student_14.reg
+const ActiveStatus14  = document.getElementById('stat14').innerHTML = student_14.status
+
+
+// Ugulu Kosisochukwu Peace - 15
+const student_15 = {
+  name: '  Ugulu Kosisochukwu Peace',
+  department: ' Linguistics',
+  level: ' ',
+  phone: '<a href="https://wa.me/+2349114509061">  09114509061</a>',
+  reg: ' 2021054022',
+  status: 'Active'
+} 
+
+const name15 = document.getElementById('name-15').innerHTML = student_15.name;
+const dept15 = document.getElementById('dept-15').innerHTML = student_15.department;
+const level15 = document.getElementById('level-15').innerHTML = student_15.level;
+const phoneNumber15 = document.getElementById('phone-number-15').innerHTML = student_15.phone;
+const reg15 = document.getElementById('reg-15').innerHTML = student_15.reg
+const ActiveStatus15  = document.getElementById('stat15').innerHTML = student_15.status
+
+// Okata Uchenna Kenneth - 16
+const student_16 = {
+  name: '  Okata Uchenna Kenneth',
+  department: ' Public Adminstration',
+  level: ' ',
+  phone: '<a href="https://wa.me/+2348161814174">  08161814174, 07049250536</a>',
+  reg: ' 2021454069',
+  status: 'Active'
+} 
+
+const name16 = document.getElementById('name-16').innerHTML = student_16.name;
+const dept16 = document.getElementById('dept-16').innerHTML = student_16.department;
+const level16 = document.getElementById('level-16').innerHTML = student_16.level;
+const phoneNumber16 = document.getElementById('phone-number-16').innerHTML = student_16.phone;
+const reg16 = document.getElementById('reg-16').innerHTML = student_16.reg
+const ActiveStatus16  = document.getElementById('stat16').innerHTML = student_16.status
+
+
+// Uguru Ezinne MarryPrecious - 17
+const student_17 = {
+  name: '  Uguru Ezinne MarryPrecious',
+  department: ' Theatre Art',
+  level: ' ',
+  phone: '<a href="https://wa.me/+2348065914061"> 08065914061</a>',
+  reg: ' 2023104049',
+  status: 'Active'
+} 
+
+const name17 = document.getElementById('name-17').innerHTML = student_17.name;
+const dept17 = document.getElementById('dept-17').innerHTML = student_17.department;
+const level17 = document.getElementById('level-17').innerHTML = student_17.level;
+const phoneNumber17 = document.getElementById('phone-number-17').innerHTML = student_17.phone;
+const reg17 = document.getElementById('reg-17').innerHTML = student_17.reg
+const ActiveStatus17  = document.getElementById('stat17').innerHTML = student_17.status
+
+
+//  Okeke Uchenna - 18
+const student_18 = {
+  name: '  Okeke Uchenna',
+  department: ' Forestry and Wildlife',
+  level: ' ',
+  phone: '<a href="https://wa.me/+2348108202823"> 08108202823</a>',
+  reg: ' 2020844031',
+  status: 'Active'
+} 
+
+const name18 = document.getElementById('name-18').innerHTML = student_18.name;
+const dept18 = document.getElementById('dept-18').innerHTML = student_18.department;
+const level18 = document.getElementById('level-18').innerHTML = student_18.level;
+const phoneNumber18 = document.getElementById('phone-number-18').innerHTML = student_18.phone;
+const reg18 = document.getElementById('reg-18').innerHTML = student_18.reg
+const ActiveStatus18  = document.getElementById('stat18').innerHTML = student_18.status
+
+
+
+// Nwankwo Nzube Victors - 19
+const student_19 = {
+  name: '  Nwankwo Nzube Victor',
+  department: ' Microbiology',
+  level: ' ',
+  phone: '<a href="https://wa.me/+2349061166109"> 09061166109</a>',
+  reg: ' 2019484104',
+  status: 'Active'
+} 
+
+const name19 = document.getElementById('name-19').innerHTML = student_19.name;
+const dept19 = document.getElementById('dept-19').innerHTML = student_19.department;
+const level19 = document.getElementById('level-19').innerHTML = student_19.level;
+const phoneNumber19 = document.getElementById('phone-number-19').innerHTML = student_19.phone;
+const reg19 = document.getElementById('reg-19').innerHTML = student_19.reg
+const ActiveStatus19  = document.getElementById('stat19').innerHTML = student_19.status
+
+// Nwume Emmanuel - 20
+const student_20 = {
+  name: '  Nwume Emmanuel',
+  department: ' Business Education',
+  level: ' ',
+  phone: '<a href="https://wa.me/+2347054935885"> 07054935885</a>',
+  reg: ' 2021194069',
+  status: 'Active'
+} 
+
+const name20 = document.getElementById('name-20').innerHTML = student_20.name;
+const dept20 = document.getElementById('dept-20').innerHTML = student_20.department;
+const level20 = document.getElementById('level-20').innerHTML = student_20.level;
+const phoneNumber20 = document.getElementById('phone-number-20').innerHTML = student_20.phone;
+const reg20 = document.getElementById('reg-20').innerHTML = student_20.reg
+const ActiveStatus20  = document.getElementById('stat20').innerHTML = student_20.status
+
+
+// James Uchechukwu Okereke - 21
+const student_21 = {
+  name: '  James Uchechukwu Okereke',
+  department: ' Business Education',
+  level: ' ',
+  phone: '<a href="https://wa.me/+2349012019644"> 09012019644</a>',
+  reg: ' 2021150489',
+  status: 'Active'
+} 
+
+const name21 = document.getElementById('name-21').innerHTML = student_21.name;
+const dept21 = document.getElementById('dept-21').innerHTML = student_21.department;
+const level21 = document.getElementById('level-21').innerHTML = student_21.level;
+const phoneNumber21 = document.getElementById('phone-number-21').innerHTML = student_21.phone;
+const reg21 = document.getElementById('reg-21').innerHTML = student_21.reg
+const ActiveStatus21  = document.getElementById('stat21').innerHTML = student_21.status
