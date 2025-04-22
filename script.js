388e8b217bd839550b3232428bd0d9c922b5a217
VALENTINE246 <somtoifeduba2@gmail.com>
2025-04-22 18:43:40 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+
+function myFunction(x) {
+  x.classList.toggle("change");
+  var navigation = x.nextElementSibling;
+  navigation.style.display = x.classList.contains("change") ? "block" : "none";
+  document.addEventListener("click", function(event) {
+    if (!x.contains(event.target) && !navigation.contains(event.target)) {
+      x.classList.remove("change");
+      navigation.style.display = "none";
+    }
+  });
+}
+
+const menuToggle = document.getElementById('menu-toggle')
+
+menuToggle.addEventListener('click', function() {  
+  document.getElementById('nav-menu').classList.toggle('active');  
+});  
+
+
+function hideSidebar() {
+  document.getElementById('nav-menu').classList.toggle('active');  
+
+}
+
+
+
+
+const logAdmin = document.getElementById('login-admin')
+
+
+
+
+
+
